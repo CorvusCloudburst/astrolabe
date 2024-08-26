a3c46fca91e56e31e775a310b487c1c15cfbe763
CorvusCloudburst <halite.ann@gmail.com>
2024-08-26 10:36:30 -0400

    update prairie caves thumbnail

--- assets/externalImages.js
+++ assets/externalImages.js
@@ -13,7 +13,7 @@ export const mapIcons = {
     daylightPrairie: {
         butterflyFields: 'https://i.imgur.com/emGO8r5.png',
         prairieVillage: 'https://i.imgur.com/gdEkgLN.png',
-        prairieCaves: 'https://i.imgur.com/lp2u5l0.png',
+        prairieCaves: 'https://i.imgur.com/XPiK4RJ.png',
         birdNest: 'https://i.imgur.com/9H4YTXn.png',
         sanctuaryIslands: 'https://i.imgur.com/ueJtJwl.png',
     },
